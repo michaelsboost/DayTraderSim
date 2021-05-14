f0f8ab95a756f679d86dc858a286f63490edbf73
Michael <>
2021-05-14 16:41:03 +0000

    cleaned up

--- js/app.js
+++ js/app.js
@@ -18,13 +18,9 @@ var activeOrder = false
 
 // balance button
 cash.onclick = function() {
-  var msg1 = "<br>All your progress will be lost!<br><br>",
-      msg2 = "Do you wish to continue?";
-  
   Swal.fire({
     title: "Warning! Clears your history!!",
     input: 'number',
-    inputLabel: msg1 + msg2,
     inputValue: '1000.00',
     showCancelButton: true,
     inputValidator: (value) => {
